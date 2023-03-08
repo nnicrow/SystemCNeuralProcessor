cf898f10aaf4624c891fcb3db12ad69ceffbd014
Shurley <55343840+DontCallMeShurley@users.noreply.github.com>
2023-03-09 01:15:04 +0300

    add read from file

--- modules/src/CD.cpp
+++ modules/src/CD.cpp
@@ -1,61 +1,71 @@
 ﻿#include "../CD.h"
-
+#include "../memory.h"
+#include <fstream>
+#include <memory.h>
 void CD::control_process()
 {
-    /*while (true)
-        {
-            wait(); // Ожидание следующего такта
-
-            // Если есть сигнал прерывания, то сохраняем текущий слой и адрес, куда нужно было передать данные
-            if (interrupt_signal.read())
-            {
-                current_layer++; // Переходим к следующему слою
-                target_address.write(result_address.read());
-                // Передаем данные на вычисление в предыдущее вычислительное ядро
-                layer_number.write(current_layer); // Сохраняем номер текущего слоя
-            }
-
-            // Если есть готовые данные на внешнем накопителе, то загружаем их в общую память
-            if (data_ready.read())
-            {
-                load_data.write(true); // Посылаем сигнал на загрузку данных
-            }
-
-            // Если есть свободные вычислительные ядра и есть данные для передачи, то передаем данные на вычисление
-            if (!is_busy && unload_data.read())
-            {
-                target_address.write(target_address.read() + 1); // Переходим к следующему нейрону в текущем слое
-                result_address.write(result_address.read() + 1);
-                // Переходим к следующему свободному адресу для записи результата
-                unload_data.write(false);
-                is_busy = true; // Устанавливаем флаг занятости вычислительных ядер
-            }
-
-            // Если есть готовый результат вычисления, то выгружаем его на выходной порт НС
-            if (result_ready.read())
-            {
-                unload_result.write(true); // Посылаем сигнал на выгрузку результата
-                is_busy = false; // Снимаем флаг занятости вычислительных ядер
-            }
-        }*/
+    while (true) {
+        if (data_ready && !load_data && !result_ready) {
+            init();
+        }
+        wait();
+    }
 }
 
 void CD::read_data_from_file()
 {
+    ifstream fin("data/weight.txt");
+    while (!fin.eof()) {
+        for (int i(0); i < 30; i++)
+            for (int j(0); j < 49; j++)
+            {
+                fin >> mem[i][j];
+            }
+        for (int i(0); i < 3; i++)
+            for (int j(0); j < 30; j++)
+            {
+                fin >> mem[i + 30][j];
+            }
+    }
 }
 
 void CD::init()
 {
+    read_data_from_file();
+    wr_o_memory = true;
+    rd_o_memory = false;
+    data_s_o_memory = 0;
+    data_len_o_memory = layer_count.read();
+    load_data = true;
 }
 
 void CD::out()
 {
+    // TODO: write data to output stream
 }
 
 void CD::memory_write()
 {
+    while (true) {
+        wait();
+        if (wr_o_memory) {
+            for (int i = 0; i < data_len_o_memory.read(); i++) {
+                memory[data_s_o_memory.read() + i] = data_out[i];
+            }
+            wr_o_memory = false;
+        }
+    }
 }
 
 void CD::memory_read()
 {
+    while (true) {
+        wait();
+        if (rd_o_memory) {
+            for (int i = 0; i < data_len_i_memory.read(); i++) {
+                data_in[i] = memory[data_addr_s_i_memory.read() + i];
+            }
+            rd_o_memory = false;
+        }
+    }
 }
