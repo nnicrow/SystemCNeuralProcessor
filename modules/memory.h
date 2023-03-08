707f6383df1c43dc30731583c2d3d9f7575aa42a
nnicrow <nnicrow@gmail.com>
2023-03-08 22:23:20 +0300

    memory fix v2

--- modules/memory.h
+++ modules/memory.h
@@ -10,24 +10,39 @@ SC_MODULE(memory)
     sc_in<bool> wr_i; // сигнал записи данных
     sc_in<bool> rd_i; // сигнал чтения данных
     sc_in<int> current_layer; // переменный параметр, указывает с каким слоем мы сейчас работаем
+    sc_in<int> data_s_i; // началльный адрес данных слоя
+    sc_in<int> data_s_o; // конечный адрес данных слоя
+    sc_in<int> data_len_i; // ширина данных
 
     // выходные сигналы
-    sc_out<int> layer_addr_s; // началльный адрес данных слоя
-    sc_out<int> layer_addr_e; // конечный адрес данных слоя
-    sc_out<int> weights_addr_s; // началльный адрес весов
-    sc_out<int> weights_addr_e; // конечный адрес весов 
+    sc_out<int> data_addr_s_o; // началльный адрес данных слоя
+    sc_out<int> data_addr_e_o; // конечный адрес данных слоя
 
+    SC_HAS_PROCESS(memory);
+
+    // чтение данных из layers_data_ под индексом номера слоя
     void mem_layer_read();
+    // запись данных в layers_data_ под индексом номера слоя
     void mem_layer_write();
+    // чтение весов в weights_data_ под индексом номера слоя + 1
     void mem_weights_read();
+
+    // инициализация. На данном этапе загружаются все веса, входной слой и колличество слоев layer_count
+    void init();
+
+    // запись весов в weights_data_ под индексом номера слоя + 1
     void mem_weights_write();
 
-    // кидаем сигнал на чтение, все ядра получают свой отрезок в памяти, который они хотят прочитать, когда они считывают, они загружают его себе.
-    // кидаем сигнал на запись данных, все данные
+    // если current_layer = 0, то инициализация данных, загрузка
+    // layer_count и mem_weights_write должны работать только в данном случае
 
     SC_CTOR(memory)
     {
         sensitive << clk.pos();
+        SC_METHOD(mem_layer_read)
+        SC_METHOD(mem_layer_write)
+        SC_METHOD(mem_weights_read)
+        SC_METHOD(init)
     }
 
 private:
--- modules/src/memory.cpp
+++ modules/src/memory.cpp
@@ -1,9 +1,21 @@
 ﻿#include "../memory.h"
 
-void memory::mem_read()
+void memory::mem_layer_read()
 {
 }
 
-void memory::mem_write()
+void memory::mem_layer_write()
+{
+}
+
+void memory::mem_weights_read()
+{
+}
+
+void memory::init()
+{
+}
+
+void memory::mem_weights_write()
 {
 }
