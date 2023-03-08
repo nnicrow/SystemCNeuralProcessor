f11a3f354071763647d7343319043d331b0026a4
nnicrow <nnicrow@gmail.com>
2023-03-08 22:35:52 +0300

    memory signals fix

--- main.cpp
+++ main.cpp
@@ -10,13 +10,23 @@ int sc_main(int argc, char* argv[])
     bus bus("bus");
     core core("core");
     memory memory("memory");
+
+    sc_signal<int> layer_count;
+    sc_signal<int> current_layer;
+
+    // memory
+    sc_signal<bool> wr_o_memory;
+    sc_signal<bool> rd_o_memory;
+    sc_signal<int> data_s_o_memory;
+    sc_signal<int> data_len_o_memory;
     
-    cout << "Hello World!";
+    sc_signal<int> data_addr_s_i_memory;
+    sc_signal<int> data_len_i_memory;
 
     sc_clock clk("clk_i", sc_time(10, SC_NS));
-    
-    //bus.clk_i(clk);
-    
+    memory.clk(clk);
+    memory.layer_count(layer_count);
+
     sc_start(sc_time(40000, SC_NS));
     return 0;
 }
